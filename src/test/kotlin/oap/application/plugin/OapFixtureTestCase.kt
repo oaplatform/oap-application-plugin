a574f3412b6ac8e73eedf9a417dcdf3056ee6bc1
igor.petrenko <igor.petrenko@xenoss.io>
2025-11-20 10:06:04 +0200

    CE-96 validation: module::dependsOn

--- src/test/kotlin/oap/application/plugin/OapFixtureTestCase.kt
+++ src/test/kotlin/oap/application/plugin/OapFixtureTestCase.kt
@@ -1,9 +1,16 @@
 package oap.application.plugin
 
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import oap.application.plugin.ref.ValidModuleDependsOnInspection
 import java.io.File
 
 abstract class OapFixtureTestCase : BasePlatformTestCase() {
+    override fun setUp() {
+        super.setUp()
+
+        myFixture.enableInspections(ValidModuleDependsOnInspection::class.java)
+    }
+
     override fun getTestDataPath(): String {
         return File("testdata/$basePath").absolutePath
     }
