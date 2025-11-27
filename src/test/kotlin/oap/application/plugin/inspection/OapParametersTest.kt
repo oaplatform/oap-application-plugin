c31206814a1774df70a0e8bf7cd906325e75a14c
igor.petrenko <igor.petrenko@xenoss.io>
2025-11-27 17:15:09 +0200

    CE-115 multiple include

--- src/test/kotlin/oap/application/plugin/inspection/OapParametersTest.kt
+++ src/test/kotlin/oap/application/plugin/inspection/OapParametersTest.kt
@@ -1,10 +1,7 @@
 package oap.application.plugin.inspection
 
-import com.intellij.psi.PsiFile
 import oap.application.plugin.OapFixtureTestCase
-import oap.application.plugin.ref.ValidModuleDependsOnInspection
 import oap.application.plugin.ref.ValidParametersInspection
-import oap.application.plugin.ref.ValidServiceDependsOnInspection
 
 class OapParametersTest : OapFixtureTestCase() {
     protected override fun setUp() {
@@ -17,4 +14,7 @@ class OapParametersTest : OapFixtureTestCase() {
         return "inspection"
     }
 
+    fun testParameterAsField() {
+
+    }
 }
